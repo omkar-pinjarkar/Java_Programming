29e88cf4cf5d8bdd1af65bc5e14a9c5a4ad32da0
omkar-pinjarkar <omkarpinjarkar2004@gmail.com>
2025-11-02 14:44:12 +0530

    Update program21_1.java

--- Assignments/Assignment_21/program21_1.java
+++ Assignments/Assignment_21/program21_1.java
@@ -6,7 +6,7 @@ class Logic
     //  Description   : To get multiplication of digits
     //  Input         : int
     //  Output        : void
-    //  Author        : Prajwal Sanjay Pansare
+    //  Author        : Omkar Nandkishor Pinjarkar
     //  Date          : 27/10/2025
     //
     //////////////////////////////////////////////////////////////
